ab1e0a4f11c64b7df819fda6219747664050508c
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-05-19 13:05:50 +0700

    fix : error dot env didn't load in render

--- src/main/java/com/pokemonmovethailand/pokemonmovebackend/PokemonmovebackendApplication.java
+++ src/main/java/com/pokemonmovethailand/pokemonmovebackend/PokemonmovebackendApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PokemonmovebackendApplication {
 
 	public static void main(String[] args) {
-		// อ่านจาก environment variables ของระบบโดยตรง
-		System.setProperty("spring.datasource.url", System.getenv("SPRING_DATASOURCE_URL"));
-		System.setProperty("spring.datasource.username", System.getenv("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("spring.datasource.password", System.getenv("SPRING_DATASOURCE_PASSWORD"));
-
 		SpringApplication.run(PokemonmovebackendApplication.class, args);
 	}
 }
